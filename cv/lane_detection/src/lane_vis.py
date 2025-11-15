1ae9ac48a5059ffd4dce6bdf3dae6d9d4a3f730e
JeremyXie123 <jeremyli.xie@mail.utoronto.ca>
2025-11-15 14:17:42 -0500

    Convert lane_vis.py

--- cv/lane_detection/src/lane_vis.py
+++ cv/lane_detection/src/lane_vis.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import rospy
+import rclpy
 from cv.msg import FloatArray
 from geometry_msgs.msg import Point
 from sensor_msgs.msg import PointCloud2, PointField
@@ -15,24 +15,24 @@ from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
 
 
 
-class FloatArrayToPointCloud2Node:
+class FloatArrayToPointCloud2Node(Node):
     def __init__(self):
-        rospy.init_node("float_array_to_pc2_node")
-        self.points2_pub = rospy.Publisher(
-            "/cv/lane_cloud", PointCloud2, queue_size=1
+        super().__init__("float_array_to_pc2_node")
+        self.points2_pub = self.create_publisher(
+            PointCloud2, "/cv/lane_cloud", 1
         )
 
         # listen for transform from camera to lidar frames
         self.tf_buffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tf_buffer)
-        
 
     def run(self):
-        self.float_array_sub = rospy.Subscriber(
-            "/cv/lane_detections", FloatArray, self.float_array_callback, queue_size=10
+        self.float_array_sub = self.create_subscription(
+            FloatArray, "/cv/lane_detections", self.float_array_callback, 10
         )
 
-        rospy.spin()
+        rclpy.spin(self)
+
     def float_array_callback(self, msg):
         points = []
         for float_list in msg.lists:
@@ -41,16 +41,15 @@ class FloatArrayToPointCloud2Node:
                 points.append(point)
 
         fields = [PointField('x', 0, PointField.FLOAT32, 1),
-          PointField('y', 4, PointField.FLOAT32, 1),
-          PointField('z', 8, PointField.FLOAT32, 1),
-          ]
+            PointField('y', 4, PointField.FLOAT32, 1),
+            PointField('z', 8, PointField.FLOAT32, 1),
+        ]
 
-
-        header = Header(frame_id='bottom_lidar_link', stamp=rospy.Time.now())
+        header = Header(frame_id='bottom_lidar_link', stamp=rclpy.Time.Time())
 
         output_msg = point_cloud2.create_cloud(header, fields, points)
 
-        trans = self.tf_buffer.lookup_transform('bottom_lidar_link', 'left_camera_link_optical', rospy.Time.now(), rospy.Duration(1))
+        trans = self.tf_buffer.lookup_transform('bottom_lidar_link', 'left_camera_link_optical', rclpy.Time.Time(), rclpy.Duration(1))
 
         output_msg = do_transform_cloud(output_msg, trans)
 
