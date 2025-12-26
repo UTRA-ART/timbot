d5a1bc62992642ce39e4df42b3f65041038b0b2d
ansonkwokys <ansonyskwok@gmail.com>
2025-12-26 16:40:13 -0500

    rviz launch files

---
+++ description/launch/rviz.launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
+def generate_launch_description():
+    
+    # Path to RViz configuration file
+    rviz_config = PathJoinSubstitution([
+        FindPackageShare("description"),
+        "rviz",
+        "espresso.rviz"
+    ])
+    
+    # RViz node
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz",
+        arguments=["-d", rviz_config],
+        output="screen"
+    )
+
+    return LaunchDescription([
+        rviz_node
+    ])
