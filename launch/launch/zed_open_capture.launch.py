254f5cde754d50d392c398330ca96c4a04b0a868
Jeremy Xie <jeremyli.xie@mail.utoronto.ca>
2026-05-04 18:21:45 -0400

    Add gain parameter to zed camera

--- launch/launch/zed_open_capture.launch.py
+++ launch/launch/zed_open_capture.launch.py
@@ -23,6 +23,12 @@ def generate_launch_description():
         description='Manual exposure value in the range 0-100',
     )
 
+    gain_arg = DeclareLaunchArgument(
+        'gain',
+        default_value='50',
+        description='Manual gain value in the range 0-100',
+    )
+
     left_image_topic_arg = DeclareLaunchArgument(
         'left_image_topic',
         default_value='/zed_node/left/image',
@@ -50,6 +56,7 @@ def generate_launch_description():
                 'video_device': LaunchConfiguration('video_device'),
                 'auto_exposure': LaunchConfiguration('auto_exposure'),
                 'exposure': LaunchConfiguration('exposure'),
+                'gain': LaunchConfiguration('gain'),
                 'left_image_topic': LaunchConfiguration('left_image_topic'),
                 'left_camera_info_topic': LaunchConfiguration('left_camera_info_topic'),
                 'depth_image_topic': LaunchConfiguration('depth_image_topic'),
@@ -62,6 +69,7 @@ def generate_launch_description():
         video_device_arg,
         auto_exposure_arg,
         exposure_arg,
+        gain_arg,
         left_image_topic_arg,
         left_camera_info_topic_arg,
         depth_image_topic_arg,
