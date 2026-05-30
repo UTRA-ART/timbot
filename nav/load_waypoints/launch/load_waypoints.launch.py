8a94a252e187be393b326726f73c00016d8bfd76
Hari Om Chadha <chadhahariom1@gmail.com>
2026-05-30 01:10:59 -0400

    gps yaw fix

--- nav/load_waypoints/launch/load_waypoints.launch.py
+++ nav/load_waypoints/launch/load_waypoints.launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
     # Default: sim_waypoints.json
     config_file_arg = DeclareLaunchArgument(
         'config_file',
-        default_value='outside.json',
+        default_value='comp_mag_test.json',
         description='Name of the waypoints JSON file in load_waypoints/jsons/'
     )
 
